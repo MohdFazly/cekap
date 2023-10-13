e6c479f0cd5bebb908c19ffcd7140e8eccfe7f6e
MohdFazly <2020819344@student.uitm.edu.my>
2023-10-13 17:55:44 +0800

    6666

---
+++ admin/display-note.php
@@ -0,0 +1,70 @@
+<?php include('connect.php'); ?>
+<?php error_reporting(0); ?>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<body>
+<title>Admin | Note </title>
+<div class="container">
+	<div class="sample">
+		<h1 class="page-header text-center">All Delivery-Note</h1>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+		<div class="sample2">
+        <!-- <a href="#addLorry" data-toggle="modal" class="pull-right btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Add Lorry</a> -->
+		</div>
+		</div>
+	</div>
+	<div style="margin-top:10px;">
+		<table class="table table-bordered" style="background-color: #fcfcfc;">
+			<colgroup>
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+            </colgroup>
+			<thead>
+				<th>Delivery Note</th>
+                <th>Lorry Number</th>
+				<th>DriverName</th>
+				<th>FromLocation</th>
+				<th>ToLocation</th>
+				<th>TotalLogs</th>
+				<th>TotalVolume</th>
+				<th>DeliveryDate</th>
+			</thead>
+			<tbody>
+				<?php
+					$sql="select * from deliverydetails";
+					$query=$conn->query($sql);
+					while($row=$query->fetch_array()){
+						?>
+						<tr>
+							<td><?php echo $row['DeliveryNote']; ?></td>
+                            <td><?php echo $row['LorryNumber']; ?></td>
+                            <td><?php echo $row['DriverName']; ?></td>
+                            <td><?php echo $row['FromLocation']; ?></td>
+                            <td><?php echo $row['ToLocation']; ?></td>
+                            <td><?php echo $row['TotalLogs']; ?></td>
+                            <td><?php echo $row['TotalVolume']; ?></td>
+                            <td><?php echo $row['DeliveryDate']; ?></td>
+                            
+							
+						</tr>
+						<?php
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
+</div>
+<!-- <?php include('add.php'); ?> -->
+</body>
+</html>
